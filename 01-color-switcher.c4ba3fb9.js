5142a30fb1b38410e2f4b8da79d7ddeb66ccead3
Bogdan2120 <bogdanvasilik2120@gmail.com>
2022-11-26 22:49:42 +0000

    Deploying to gh-pages from @ Bogdan2120/goit-js-hw-09@d2373854811adee2555a26f8b13af391b751df42 🚀

---
+++ 01-color-switcher.c4ba3fb9.js
@@ -0,0 +1,2 @@
+!function(){var t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.body};t.start.addEventListener("click",(function(){o=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.start.disabled=!0})),t.stop.addEventListener("click",(function(){clearInterval(o),t.start.disabled=!1}));var o=null}();
+//# sourceMappingURL=01-color-switcher.c4ba3fb9.js.map
